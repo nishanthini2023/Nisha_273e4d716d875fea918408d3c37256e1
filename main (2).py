ea57fa0eb7596c6df1b3a8349968d03fe0a1e294
nishanthini2023 <145525386+nishanthini2023@users.noreply.github.com>
2023-09-27 15:31:27 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,13 +1,24 @@
-
-def isleapyear(year):
-   if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-     return True
-   else:
-     return False
-
-year = int(input("Enter a year :"))
-
-if isleapyear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+class Student:
+  def __init__(self, name, roll_number, cgpa):
+   self.name = name 
+   self.roll_number = roll_number
+   self.cgpa = cgpa
+def sort_students(student_list):
+  sorted_students = sorted(student_list,key=lambda student: 
+                                student.cgpa,reverse = True)
+  return sorted_students
+students = [ Student("prabha","5052",7.8),
+               Student("hanish","5051",8.9),
+               Student("jaya","5040",7.0),
+               Student("nithya","5045",6.0),
+               Student("sowmiya","5060",8.5),
+               Student("kiruthi","5030",7.5)
+             ]
+sorted_students = sort_students(students)
+print("\n\n")
+print(" ************************************************** ")
+print("\tNAME \t\t\t\t REGNO \t\t\t PERCENTAGE ")
+print(" ************************************************** ")
+for student in sorted_students:
+  print("Name: {}, \t Roll Number: {}, \tCGPA: {}" .format(student.name, 
+                                                    student.roll_number, student.cgpa))
